71b306c024c2c6476528814eeae431d4da5ba2eb
Siro <yamapeachsan@gmail.com>
2025-09-15 19:52:20 -0600

    changing files a bit

---
+++ Content.Server/_Monkestation/Audio/ICassettes/ICassetteSystem.cs
@@ -0,0 +1,9 @@
+using Content.Shared._Monkestation.Audio.ICassettes;
+
+namespace Content.Server._Monkestation.Audio.ICassettes
+{
+    public sealed class CassetteSystem : ISharedCassetteSystem
+    {
+
+    }
+}
---
+++ Content.Shared/_Monkestation/Audio/ICassettes/ISharedCassetteSystem.cs
@@ -0,0 +1,11 @@
+using Robust.Shared.GameStates;
+using Robust.Shared.Network;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared._Monkestation.Audio.ICassettes
+{
+    public abstract class ISharedCassetteSystem : EntitySystem
+    {
+        // Empty for now—server will inherit
+    }
+}
